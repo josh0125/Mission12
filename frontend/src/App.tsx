618045ab444597a472298e4471095f64d67f08cc
Joshua Allen <joshutallen99@gmail.com>
2023-03-22 12:44:42 -0600

    updated react and bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/data';
+
+function Header() {
+  return(
+    <div className='container m-4'>
+        <h2>Welcome to NCAA March Madness</h2>
+        <p>This sites shows the list of all NCAA basketball teams, their mascots, and location.</p>
+    </div>
+  );
+}
+class Team extends React.Component < {school: string, name: string, city: string, state: string} > {
+  render() {
+    const oneTeam = this.props
+
+    return(    
+      
+          <div className='card text-dark m-5 col-3'>
+            <h2>{oneTeam.school}</h2>
+            <span>{oneTeam.name} </span>
+            <span>{oneTeam.city}, {oneTeam.state}</span>
+          </div>
+    );
+  }
+}
+
+function TeamList() {
+  return(
+    <div className='container'>
+        <div className='row'>
+          {data.teams.map(oneTeam => <Team {...oneTeam}/>)}
+        </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Header></Header>
+          <TeamList></TeamList>
+        </div>
       </header>
+      
     </div>
   );
 }
